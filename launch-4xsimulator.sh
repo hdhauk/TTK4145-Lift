a42ed2ebfed1a549be0faf2d61ffda54399ff9fc
halvor <halvor@HP-Ubuntu>
2017-01-19 16:50:58 +0100

    tweaked startup script

--- launch-4xsimulator.sh
+++ launch-4xsimulator.sh
@@ -10,16 +10,16 @@ gnome-terminal -e "rdmd sim_server.d" --geometry 50x9+200+700
 cd ../../..
 
 echo "Waiting..."
-sleep .5
+sleep 1
 echo "..for.."
-sleep .5
+sleep 1
 echo "..the.."
-sleep .5
+sleep 1
 echo "..simulators.."
-sleep .5
+sleep 1
 echo ".. to boot!"
 
-gnome-terminal -e './ttk4145-elevator -sim 53566' --geometry 90x9+680+100
-gnome-terminal -e './ttk4145-elevator -sim 53567' --geometry 90x9+680+300
-gnome-terminal -e './ttk4145-elevator -sim 53568' --geometry 90x9+680+500
-gnome-terminal -e './ttk4145-elevator -sim 53569' --geometry 90x9+680+700
+gnome-terminal -e './ttk4145-elevator -sim 53566 -id sim53566' --geometry 90x9+680+100
+gnome-terminal -e './ttk4145-elevator -sim 53567 -id sim53567' --geometry 90x9+680+300
+gnome-terminal -e './ttk4145-elevator -sim 53568 -id sim53568' --geometry 90x9+680+500
+gnome-terminal -e './ttk4145-elevator -sim 53569 -id sim53569' --geometry 90x9+680+700
