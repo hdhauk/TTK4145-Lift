24c7adace8a77977f8be0228dfcbd66546b4daa2
halvor <halvor@HP-Ubuntu>
2017-03-01 18:13:24 +0100

    travis

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,3 @@
 wget http://www.comedi.org/download/comedilib-0.10.2.tar.gz
 tar -xvzf comedilib-0.10.2.tar.gz
-cd comedilib-0.10.2 && ./configure && make && sudo  make install
+cd comedilib-0.10.2 && ./configure && make && sudo make install
